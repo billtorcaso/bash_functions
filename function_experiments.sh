3c7db88ca442aded2d5e7138250540378d3c4cd5
Bill Torcaso <torcasobill@gmail.com>
2021-12-31 17:09:13 -0500

    Centralize all github repos under "$HOME/Code/github-here"

--- function_experiments.sh
+++ function_experiments.sh
@@ -11,14 +11,17 @@ function fns_refresh()  {
     source ${TMP_FNS:-$HOME/bin/function_experiments.sh};
 }
 
-function weh()  { Code wagtail-experiments-here/"$1"; }
-function btoh() { weh bto-here/bto/"$1"; }
+function gth()  { Code github-here/"$1"; }
+function btoh() { gth bto_v2/"$1"; }
 
 function ec2sh(){ 
     clear;
-    ssh -i "$HOME/.ssh/key-BTO-2021-12-09-V2.pem" \
-           ubuntu@ec2-3-133-238-192.us-east-2.compute.amazonaws.com \
-           "$@";
+    (   cd $HOME/tmp;
+        ssh -i "$HOME/.ssh/key-BTO-2021-12-09-V2.pem" \
+            ubuntu@ec2-3-133-238-192.us-east-2.compute.amazonaws.com \
+            "$@";
+    )
+    pwd
 }
 
 btosh() { echo 1>&2 "This function name iss obsolete; use 'ec2sh'."; }
