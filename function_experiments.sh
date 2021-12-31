bad76cd70116dee96e221e0c24a7b54628ecd94d
Bill Torcaso <torcasobill@gmail.com>
2021-12-31 14:38:57 -0500

    Initial version.

---
+++ function_experiments.sh
@@ -0,0 +1,28 @@
+# Temporary things, before I decide to promote them.
+
+function fns_begin()  {
+    source $HOME/m1.setup  && \
+    source $HOME/truly_generic.bash_functions && \
+    source ${TMP_FNS:-$HOME/bin/function_experiments.sh};
+}
+
+function fns_refresh()  {
+    source $HOME/truly_generic.bash_functions && \
+    source ${TMP_FNS:-$HOME/bin/function_experiments.sh};
+}
+
+function weh()  { Code wagtail-experiments-here/"$1"; }
+function btoh() { weh bto-here/bto/"$1"; }
+
+function ec2sh(){ 
+    clear;
+    ssh -i "$HOME/.ssh/key-BTO-2021-12-09-V2.pem" \
+           ubuntu@ec2-3-133-238-192.us-east-2.compute.amazonaws.com \
+           "$@";
+}
+
+btosh() { echo 1>&2 "This function name iss obsolete; use 'ec2sh'."; }
+
+btocurl()   {
+    curl ec2-3-133-238-192.us-east-2.compute.amazonaws.com:${1:-8000};
+}
