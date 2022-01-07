d8e92d818b60aeb363ee1b7b1aadfc5506143b58
Ubuntu@BillTorcaso <torcasobill@gmail.com>
2022-01-07 18:24:30 +0000

    1) Add a guard for adding $HOME/.local/bin to PATH 2) revise bto2 to arrive in the billtorcaso_org directory.

--- function_experiments.sh
+++ function_experiments.sh
@@ -1,5 +1,11 @@
 # Temporary things, before I decide to promote them.
 
+[[ -d $HOME/.local/bin ]] && \
+{
+	PATH="$PATH:$HOME/.local/bin"
+} || \
+{ echo 1>&2 "Warning: can't find $HOME/.local/bin"; }
+
 export _BT_BTO_KEYPAIR="$HOME/.ssh/key-BTO-2021-12-09-V2.pem"
 export _BT_BTO_PUBLIC_IP=ec2-3-142-231-109.us-east-2.compute.amazonaws.com
 
@@ -16,7 +22,7 @@ function fns_refresh()  {
 
 function gth()  { Code github-here/"$1"; }
 function btoh() { gth bto_v2/"$1"; }
-function bto2() { Code bto-2022-01-06-here/bto-2022-01-06/"$1"; }
+function bto2() { Code bto-2022-01-06-here/bto-2022-01-06/billtorcaso_org/"$1"; }
 
 function ec2sh(){ 
     clear;
